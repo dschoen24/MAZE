81cac2d28e80ce26fe7af7e7b1297c7b8563785c
V-MalM <81383838+V-MalM@users.noreply.github.com>
2021-09-07 18:02:47 -0500

    changes

--- Vasantha/Maze_Dashboard/app.py
+++ Vasantha/Maze_Dashboard/app.py
@@ -1,39 +1,402 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, redirect, request
+import json
+from flask.json import jsonify
 from sqlalchemy import create_engine
 from config1 import db_user, db_pass #credentials to connect to postgresql database
+import pandas as pd
+import os
+
+print(os.getcwd())
 
 app = Flask(__name__)
 
 # Creating an engine to connect to postgresql database 'employee_info'
 my_engine = create_engine(f'postgresql://{db_user}:{db_pass}@localhost:5432/MAZE')
+
 # creating connection
 connection = my_engine.connect()
 
-# fetch salaries of all employees from the salaries table
+# fetch states 
 get_states = "select distinct state_fips, sabbr, sname from  maze_data order by sabbr"
+               
+               
+us_museum_query = "select state_fips, sabbr, museum_type_id, museum_type , "\
+            " count(museum_id) as total_museums "\
+            " from maze_data group by state_fips, sabbr, museum_type_id, "\
+            " museum_type order by sabbr"
+us_museum_df = pd.read_sql(us_museum_query, connection)
 
-# SELECT array_to_string(array( SELECT row_to_json(us_states.*) FROM us_states),', ') as jsonData;
+
+total_museums = 0
+total_zoo_count	= 0
+total_aqua_count = 0
+total_nc_count = 0
+prev_county = 0
+ctr = 0
+json_state_string = []
+
+for index,row in us_museum_df.iterrows():
+    if index == 0 :
+        prev_state = row['state_fips']
+              
+    if row['state_fips'] == prev_state :
+        if ctr == 0:
+            curr_state_fips =  row['state_fips'] 
+            curr_sabbr =  row['sabbr'] 
+        
+        if (row['museum_type_id'] == 1 or \
+            row['museum_type_id'] == 3 or \
+            row['museum_type_id'] == 4 or \
+            row['museum_type_id'] == 5 or \
+            row['museum_type_id'] == 7 or \
+            row['museum_type_id'] == 8 or \
+            row['museum_type_id'] == 9) :
+            total_museums = total_museums + row['total_museums']
+            prev_state =  row['state_fips'] 
+        elif row['museum_type_id'] == 2 :
+            total_nc_count = row['total_museums']    
+        elif row['museum_type_id'] == 6 :
+            total_zoo_count = row['total_museums']     
+        elif row['museum_type_id'] == 10 :
+            total_aqua_count = row['total_museums']               
+    else:
+        totals_state_dict = {'state_fips':curr_state_fips,'sabbr':curr_sabbr, 'total_museums':total_museums, \
+            'total_zoo_count': total_zoo_count, 'total_aqua_count':total_aqua_count,\
+            'total_nc_count':total_nc_count}
+          
+        json_state_string.append(totals_state_dict)
+
+        prev_state =  row['state_fips']     
+        ctr = 0  
+        total_museums = 0
+        total_zoo_count	= 0
+        total_aqua_count = 0
+        total_nc_count = 0
+        if (row['museum_type_id'] == 1 or \
+            row['museum_type_id'] == 3 or \
+            row['museum_type_id'] == 4 or \
+            row['museum_type_id'] == 5 or \
+            row['museum_type_id'] == 7 or \
+            row['museum_type_id'] == 8 or \
+            row['museum_type_id'] == 9) :
+            total_museums = row['total_museums']
+        elif row['museum_type_id'] == 2 :
+            total_nc_count = row['total_museums']    
+        elif row['museum_type_id'] == 6 :
+            total_zoo_count = row['total_museums']     
+        elif row['museum_type_id'] == 10 :
+            total_aqua_count = row['total_museums']
+    if index+1 == len(us_museum_df) :   
+
+        totals_state_dict = {'state_fips':row['state_fips'], 'sabbr':row['sabbr'], 'total_museums':total_museums, \
+        'total_zoo_count': total_zoo_count, 'total_aqua_count':total_aqua_count,\
+        'total_nc_count':total_nc_count}
+   
+        json_state_string.append(totals_state_dict)
+
+    ctr = ctr + 1   
+    curr_state_fips =  row['state_fips'] 
+    curr_sabbr =  row['sabbr']  
+us_list = json_state_string
+ 
+
+us_all_museum_query = "select museum_name, latitude, longitude, state_fips,county_fips from maze_data "\
+" where latitude != 0 order by county_fips"
+us_all_museum_df = pd.read_sql(us_all_museum_query, connection)
+
+us_all_result = us_all_museum_df.to_json(orient="records")
+us_mus_list = json.loads(us_all_result)
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/API")
+def API():
+   return jsonify(us_list)
+
+@app.route("/APIM")
+def APIM():
+# may be create a json file here 
+    # file_path = ('data.js')
+    # print (file_path)
+
+    # jsonString = "data =" + json.dumps(us_mus_list) 
+    # jsonFile = open(file_path, "w")
+    # jsonFile.write(jsonString)
+    # jsonFile.close()
+
+    return jsonify(us_mus_list)   
+
+# @app.route("/APIP")
+# def APIP():   
+
+
+@app.route("/APIREV/<st>")
+def st(st):
+    if (str(st) == "0"):
+        get_top_reve_making_query = "select distinct legal_name, revenue from maze_data where "\
+        "revenue is not null "\
+        "order by revenue desc limit 10 "        
+    else:
+        get_top_reve_making_query = "select distinct legal_name, revenue from maze_data where "\
+        "state_fips  ="+str(st)+" and revenue is not null "\
+        "order by revenue desc limit 10 "
+    
+    get_top_reve_making = pd.read_sql(get_top_reve_making_query,connection)
+    top_reve_making = get_top_reve_making.to_json(orient="records")
+    top_reve_making = json.loads(top_reve_making)
+
+    return jsonify(top_reve_making)
+
+
+
+@app.route("/APIHM")
+def APIHM():
+
+    us_muse_type_query = "select * from maze_mus_data_all_states_grouped_by_mus_type"
+    us_muse_types = pd.read_sql(us_muse_type_query, connection)
+    # total_museums = 0
+    prev_state = 0
+    ctr = 0
+    state_dict = {}
+    state_coord = []
+    locations = []
+
+    for index,row in us_muse_types.iterrows():
+        if index == 0 :
+            prev_state = row['state_fips']
+                
+        if row['state_fips'] == prev_state :
+            if ctr == 0:
+                curr_state_fips =  row['state_fips'] 
+                curr_sabbr =  row['sabbr'] 
+                curr_lat = row['latitude']
+                curr_lon = row['longitude']
+
+            state_dict[row['museum_type']] = row['total_museums']    
+        else:        
+            # print(curr_state_fips, curr_sabbr, curr_lat, curr_lon)
+            state_coord.append(curr_state_fips)
+            state_coord.append(curr_sabbr)
+            state_coord.append(curr_lat)
+            state_coord.append(curr_lon)
+            state_dict['coordinates'] = state_coord
+            locations.append(state_dict)
+
+            prev_state =  row['state_fips']     
+            ctr = 0  
+            # total_museums = 0
+            state_coord = []
+            state_dict = {}
+
+            
+        if index+1 == len(us_muse_types) :   
+            # print(index+1,len(us_muse_types))    
+            # print('last',row['state_fips'], row['sabbr'], row['latitude'], row['longitude'])
+            state_coord.append(row['state_fips'])
+            state_coord.append(row['sabbr'])
+            state_coord.append(row['latitude'])
+            state_coord.append(row['longitude'])
+            state_dict['coordinates'] = state_coord
+            locations.append(state_dict)
+            
+
+        ctr = ctr + 1   
+        curr_state_fips =  row['state_fips'] 
+        curr_sabbr =  row['sabbr']
+        curr_lat = row['latitude']
+        curr_lon = row['longitude'] 
+        state_dict[row['museum_type']] = row['total_museums']  
+
+    return jsonify(locations)  
+
+
+@app.route("/API/<state>")
+def state(state):
+    # state = 2
+    state_museum_query = "select county_fips, county, museum_type_id, museum_type , "\
+        "count(museum_id) as total_museums "\
+                        " from maze_data where state_fips ="+str(state)+" group by county_fips, "\
+                        " county, museum_type_id, museum_type  "\
+                        " order by county"
+    s_museum_df = pd.read_sql(state_museum_query, connection)
+    
+
+    total_museums = 0
+    total_zoo_count	= 0
+    total_aqua_count = 0
+    total_nc_count = 0
+    prev_county = 0
+    ctr = 0
+    json_county_string = []
+
+    for index,row in s_museum_df.iterrows():
+        if index == 0 :
+            prev_county = row['county_fips']
+                
+        if row['county_fips'] == prev_county :
+            if ctr == 0:
+                curr_county =  row['county'] 
+            
+            if (row['museum_type_id'] == 1 or \
+                row['museum_type_id'] == 3 or \
+                row['museum_type_id'] == 4 or \
+                row['museum_type_id'] == 5 or \
+                row['museum_type_id'] == 7 or \
+                row['museum_type_id'] == 8 or \
+                row['museum_type_id'] == 9) :
+                total_museums = total_museums + row['total_museums']
+                prev_county =  row['county_fips'] 
+            elif row['museum_type_id'] == 2 :
+                total_nc_count = row['total_museums']    
+            elif row['museum_type_id'] == 6 :
+                total_zoo_count = row['total_museums']     
+            elif row['museum_type_id'] == 10 :
+                total_aqua_count = row['total_museums']               
+        else:
+            totals_dict = {'county_x':curr_county, 'total_museums':total_museums, \
+                'total_zoo_count': total_zoo_count, 'total_aqua_count':total_aqua_count,\
+                'total_nc_count':total_nc_count}
+            # print (totals_dict)    
+            json_county_string.append(totals_dict)
+
+            # print (curr_county, total_museums, total_zoo_count, total_aqua_count,total_nc_count )
+            prev_county =  row['county_fips']     
+            ctr = 0  
+            total_museums = 0
+            total_zoo_count	= 0
+            total_aqua_count = 0
+            total_nc_count = 0
+            if (row['museum_type_id'] == 1 or \
+                row['museum_type_id'] == 3 or \
+                row['museum_type_id'] == 4 or \
+                row['museum_type_id'] == 5 or \
+                row['museum_type_id'] == 7 or \
+                row['museum_type_id'] == 8 or \
+                row['museum_type_id'] == 9) :
+                total_museums = row['total_museums']
+            elif row['museum_type_id'] == 2 :
+                total_nc_count = row['total_museums']    
+            elif row['museum_type_id'] == 6 :
+                total_zoo_count = row['total_museums']     
+            elif row['museum_type_id'] == 10 :
+                total_aqua_count = row['total_museums']
+        if index+1 == len(s_museum_df) :   
+
+            # print (row['county'] , total_museums, total_zoo_count, total_aqua_count,total_nc_count )
+
+            totals_dict = {'county_x':row['county'], 'total_museums':total_museums, \
+            'total_zoo_count': total_zoo_count, 'total_aqua_count':total_aqua_count,\
+            'total_nc_count':total_nc_count}
+            # print (totals_dict)    
+            json_county_string.append(totals_dict)
+
+        ctr = ctr + 1   
+        curr_county = row['county']   
+    return jsonify(json_county_string)
+
+
+@app.route("/API/<state>/<county>")
+def county(state,county):
+    # county = 41017
+    county_museum_query = "select city_phyloc, museum_type_id, museum_type , count(museum_id) as total_museums "\
+                            " from maze_data where county_fips ="+str(county)+" group by city_phyloc, "\
+                            "museum_type_id, museum_type order by city_phyloc"
+    ct_museum_df = pd.read_sql(county_museum_query, connection)
+
+    total_museums = 0
+    total_zoo_count	= 0
+    total_aqua_count = 0
+    total_nc_count = 0
+    prev_city = 0
+    ctr = 0
+    json_city_string = []
+
+    for index,row in ct_museum_df.iterrows():
+        if index == 0 :
+            prev_city = row['city_phyloc']
+                
+        if row['city_phyloc'] == prev_city :
+            if ctr == 0:
+                curr_city =  row['city_phyloc'] 
+            
+            if (row['museum_type_id'] == 1 or \
+                row['museum_type_id'] == 3 or \
+                row['museum_type_id'] == 4 or \
+                row['museum_type_id'] == 5 or \
+                row['museum_type_id'] == 7 or \
+                row['museum_type_id'] == 8 or \
+                row['museum_type_id'] == 9) :
+                total_museums = total_museums + row['total_museums']
+                prev_city =  row['city_phyloc'] 
+            elif row['museum_type_id'] == 2 :
+                total_nc_count = row['total_museums']    
+            elif row['museum_type_id'] == 6 :
+                total_zoo_count = row['total_museums']     
+            elif row['museum_type_id'] == 10 :
+                total_aqua_count = row['total_museums']               
+        else:
+            city_totals_dict = {'city_phyloc':curr_city, 'total_museums':total_museums, \
+                'total_zoo_count': total_zoo_count, 'total_aqua_count':total_aqua_count,\
+                'total_nc_count':total_nc_count}
+            # print (totals_dict)    
+            json_city_string.append(city_totals_dict)
+
+            # print (curr_county, total_museums, total_zoo_count, total_aqua_count,total_nc_count )
+            prev_city =  row['city_phyloc']     
+            ctr = 0  
+            total_museums = 0
+            total_zoo_count	= 0
+            total_aqua_count = 0
+            total_nc_count = 0
+            if (row['museum_type_id'] == 1 or \
+                row['museum_type_id'] == 3 or \
+                row['museum_type_id'] == 4 or \
+                row['museum_type_id'] == 5 or \
+                row['museum_type_id'] == 7 or \
+                row['museum_type_id'] == 8 or \
+                row['museum_type_id'] == 9) :
+                total_museums = row['total_museums']
+            elif row['museum_type_id'] == 2 :
+                total_nc_count = row['total_museums']    
+            elif row['museum_type_id'] == 6 :
+                total_zoo_count = row['total_museums']     
+            elif row['museum_type_id'] == 10 :
+                total_aqua_count = row['total_museums']
+        if index+1 == len(ct_museum_df) :   
+
+            # print (row['county'] , total_museums, total_zoo_count, total_aqua_count,total_nc_count )
+
+            city_totals_dict = {'city_phyloc':row['city_phyloc'], 'total_museums':total_museums, \
+            'total_zoo_count': total_zoo_count, 'total_aqua_count':total_aqua_count,\
+            'total_nc_count':total_nc_count}
+            # print (totals_dict)    
+            json_city_string.append(city_totals_dict)
+
+        ctr = ctr + 1   
+        curr_city = row['city_phyloc']   
+    return jsonify(json_city_string)
+
+
+# #############################################################################################
+@app.route("/API/<state>/<county>/<city>")
+def city(state,county,city):
+    state = state
+    # county=20001
+    city_museum_query = "select  museum_type, count(museum_id) as total_museums from maze_data "\
+                        "where county_fips = "+str(county)+ " and "\
+                            "city_phyloc ='"+str(city)+"' group by museum_type "\
+                        " order by museum_type"
+    city_museum_df = pd.read_sql(city_museum_query, connection)
+
+    cit_result = city_museum_df.to_json(orient="records")
+    city_list = json.loads(cit_result)
+    return jsonify(city_list)
+###############################################################################################    
     
 @app.route("/dashboard",methods=["GET", "POST"])
 def dashboard():
-    # Fetching records. we are only creating one record and updating it with each scrape 
-    # This will fetch just one record (in the form od a list) into 'results' because the collection has only one
-    # Alternatively we can use find_one.
-    state_list = connection.execute(get_states)
-    us_museum_list = connection.execute("select state_fips, sabbr, count(museum_id) as total_museums from public.maze_data where museum_type_id not in (2,6) group by state_fips, sabbr order by total_museums desc")
-    
-    records_dict= ""
-    
-    # get init dashboard ##################################    
-    # print(records)
-    
-    records_dict=[{ 'state': each_record[1], 'total_museums': each_record[2] } for each_record in us_museum_list]
-	
-
-    #######################################################
 
     selected_state =  ""
     selected_county = ""
@@ -47,22 +410,34 @@ def dashboard():
         selected_state = request.form["SelState"]
         selected_county = request.form["SelCounty"]
         selected_city = request.form["SelCity"]
+        # selected_mtype = request.form.getlist('mtype')
+        # print(selected_mtype)
+
         if selected_state != "":
-            get_counties = "select distinct county_fips, county from maze_data where state_fips="+selected_state+'order by county '
+            get_counties = "select distinct sname, county_fips, county from maze_data where latitude > 0  and state_fips="+selected_state+' order by county '
 
             county_list = connection.execute(get_counties)
         if selected_county != "":
-            get_cities = "select distinct city_phyloc from maze_data where county_fips="+selected_county+'order by city_phyloc '
+            get_cities = "select distinct county,  city_phyloc from maze_data where latitude > 0 and county_fips="+selected_county+' order by city_phyloc '
+            print(get_cities)
             city_list = connection.execute(get_cities)
 
+    
     # the results are rendered and displayed in HTML 
-    return render_template("dashboard.html", state_list=state_list,
+    mus_types = connection.execute("select * from us_museum_types order by museum_type")
+
+    get_states = "select distinct state_fips, sabbr, sname from  maze_data where latitude > 0 order by sabbr"
+    state_list = connection.execute(get_states)
+
+       
+    return render_template("dashboard.html",  mus_types=mus_types,state_list=state_list,
     sel_state=selected_state, county_list=county_list, sel_county=selected_county, 
-    city_list=city_list, sel_city=selected_city, records_dict=records_dict)
+    city_list=city_list, sel_city=selected_city)
+
 
 @app.route("/maps")
 def maps():
     return render_template("maps.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
