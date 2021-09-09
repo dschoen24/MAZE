836617073e391ea5056a329965192644673fb841
V-MalM <81383838+V-MalM@users.noreply.github.com>
2021-09-08 20:32:02 -0500

    updated config

--- Vasantha/Maze_Dashboard/app.py
+++ Vasantha/Maze_Dashboard/app.py
@@ -3,7 +3,7 @@ import json
 from flask.json import jsonify
 from sqlalchemy import create_engine
 # from config1 import db_user, db_pass #credentials to connect to postgresql database
-from config1 import *
+# from config1 import *
 import pandas as pd
 import os
 
@@ -15,7 +15,11 @@ app = Flask(__name__)
 # Creating an engine to connect to postgresql database 'employee_info'
 
 # my_engine = create_engine(f'postgresql://{db_user}:{db_pass}@localhost:5432/MAZE')
-
+server='ec2-54-147-126-173.compute-1.amazonaws.com'
+port='5432'
+db_user='ihrwvwmlryxtps'
+db='d84rgddae1lb9t'
+db_pass=os.environ.get('DB_PASS', '')
 db_url=f'{db_user}:{db_pass}@{server}:{port}/{db}'
 # db_url=#the environment variable called POSTGRES_URL
 my_engine=create_engine(db_url)
